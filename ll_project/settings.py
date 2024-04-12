7ff5ac7080f6aa48ad8f388d971fda47f11ce883
dskarachkov <dskarachkov@gmail.com>
2024-04-12 15:38:32 +0300

    готов к развертыванию

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -134,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'learning_logs:index'
 LOGOUT_REDIRECT_URL = 'learning_logs:index'
 LOGIN_URL = 'accounts:login'
+
+# Настройки Heroku
+import django_heroku
+django_heroku.settings(locals())
